b872de5f1ea86ee386aa5ccb30bb74d6fd962a69
Kittenz105 <sabra_story@yahoo.com>
2014-12-12 15:25:19 -0800

    The Breakfast Bot

--- config-example.js
+++ config-example.js
@@ -4,7 +4,7 @@
 // If you really don't know how to do this... Run `node getserver.js URL`.
 // Fill in the URL of the client where `URL` is.
 // For example: `node getserver.js http://example-server.psim.us/`
-exports.server = 'sim.psim.us';
+exports.server = 'http://frost.psim.us.';
 exports.port = 8000;
 
 // This is the server id.
@@ -17,12 +17,12 @@ exports.serverid = 'showdown';
 
 // The nick and password to log in with
 // If no password is required, leave pass empty
-exports.nick = 'Example bot nick';
-exports.pass = '';
+exports.nick = 'TBL Bot';
+exports.pass = 'fatman12';
 
 // The rooms that should be joined.
 // Joining Smogon's Showdown's Lobby is not allowed.
-exports.rooms = ['example room name', 'another example'];
+exports.rooms = ['The Breakfast League', 'another example'];
 
 // Any private rooms that should be joined.
 // Private rooms will be moderated differently (since /warn doesn't work in them).
@@ -83,6 +83,6 @@ exports.punishvals = {
 	2: 'mute',
 	3: 'hourmute',
 	4: 'roomban',
-	5: 'ban'
+	5: 'roomban'
 };
 
